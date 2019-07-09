49312306cb37d402a8617da0645f1d97c89d217a
Pablo Calofatti <45133535+pablocalofatti@users.noreply.github.com>
2019-07-09 20:23:16 -0300

    Update email.js

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports ={
-    user: 'f01cae39ec79dd',
-    password: 'a458a54cad1cdc',
-    host: 'smtp.mailtrap.io',
-    port: '2525'
+    user: 'your mailtrap user',
+    password: 'your mailtrap pass',
+    host: 'mailtrap host',
+    port: 'mailtrap port'
 }
